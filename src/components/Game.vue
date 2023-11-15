6efd9308f82d72018efaf6271e49a8a06fe26853
windlil <spacesouls@163.com>
2023-11-15 21:14:02 +0800

    chore: update

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,19 +1,17 @@
 <script setup lang="ts">
 import Map from './Map.vue'
+import Player from './Player.vue'
 </script>
 
 <template>
   <div class="game">
     <Map></Map>
+    <Player></Player>
   </div>
 </template>
 
 <style scoped lang="scss">
 .game {
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  position: relative;
 }
 </style>
---
+++ src/components/Player.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Player from '../assets/keeper.png'
+
+const STEP = 50
+
+const player = {
+  x: 1,
+  y: 1,
+}
+
+const position = {
+  top: STEP * player.x + 'px',
+  left: STEP * player.y + 'px',
+}
+</script>
+
+<template>
+  <div class="keeper box" :style="position">
+    <img :src="Player" alt="">
+  </div>
+</template>
+
+<style scoped lang="scss">
+.keeper {
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+</style>
